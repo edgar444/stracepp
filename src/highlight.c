963ec03f8765ddd5ad9738ab2886026739d938be
Edgars Dragon <edgar444@users.noreply.github.com>
2020-08-01 17:57:31 +0300

    src: inc perf by use of many tighter ctx-dependent loops

--- src/highlight.c
+++ src/highlight.c
@@ -22,17 +22,10 @@
 #define DYE_SYS   "\033[38;5;103m"  /* pale dark blue  */
 
 #define CTX_PID   (1U<<0)
-#define CTX_INT   (1U<<1)
-#define CTX_ENUM  (1U<<2)
-#define CTX_MARK  (1U<<3)
-#define CTX_CMNT  (1U<<4)
-#define CTX_STR   (1U<<5)
 #define CTX_SYS   (1U<<6)
 
 #define HAD_PID   (1U<<0)
-#define HAD_ESC   (1U<<1)
 #define HAD_SYS   (1U<<2)
-#define HAD_CMNT  (1U<<3)
 
 struct syn_premeta {
 	const char *dye, *dyed;
@@ -45,7 +38,8 @@ int main(void) {
 	struct syn_premeta p = {0};
 	p.dye = p.dyed = DYE_NONE;
 
-	for(int c = 0; p.prev = c, c = getchar(), c != -1; printf("%s%c", p.dye != p.dyed ? p.dyed = p.dye : "", c)) {
+	for(int c = '\n'; p.prev = c, c = getchar(), c != -1; printf("%s%c", p.dye != p.dyed ? p.dyed = p.dye : "", c)) {
+again:
 		/* next entry */
 		if(c == '\n') {
 			p.dye = DYE_NONE;
@@ -53,34 +47,11 @@ int main(void) {
 			continue;
 		}
 
-		/* exit context */
-
 		if(!p.ctx) goto noctx;
 
-		if(p.ctx == CTX_CMNT && c == '/' && p.prev == '*') {
-			p.had |= HAD_CMNT;
-			continue;
-		} else if(p.ctx == CTX_STR) {
-			if(c == '\\') {
-				p.had |= HAD_ESC;
-				putchar(c);
-				putchar(c = getchar());
-				c = getchar();
-				continue;
-			} else if (p.had & HAD_ESC) {
-				p.had &= ~HAD_ESC;
-				continue;
-			}
-		}
-
+		/* exit context */
 		if(
-			(p.ctx == CTX_MARK) ||
-			(p.ctx == CTX_INT && !((c >= '0' && c <= '9') || c == 'x' || (c >= 'a' && c <= 'f'))) ||
-			(p.ctx == CTX_ENUM && !((c >= 'A' && c <= 'Z') || c == '_')) ||
-			(p.ctx == CTX_STR && p.prev == '"' && ~p.had & HAD_ESC) ||
-			(p.ctx == CTX_CMNT && p.had & HAD_CMNT) ||
-			(p.ctx == CTX_SYS && p.had & HAD_SYS && !((c >= 'a' && c <= 'z') || (c >= '0' && c <= '9') || c == '_')) ||
-			(p.ctx == CTX_PID && c == ' ')
+			(p.ctx == CTX_SYS && p.had & HAD_SYS && !((c >= 'a' && c <= 'z') || (c >= '0' && c <= '9') || c == '_'))
 		) {
 			p.ctx = 0;
 			p.dye = DYE_NONE;
@@ -102,34 +73,43 @@ noctx:
 		}
 
 		/* pid (field 1) */
-		if(~p.had & HAD_PID && (p.had |= HAD_PID)) {
-			if(c >= '0' && c <= '9' && (p.ctx = CTX_PID))
-				p.dye = DYE_PID;
+		if(p.prev == '\n' && c >= '0' && c <= '9') {
+			printf(DYE_PID "%c", c);
+			while(p.prev = c, c = getchar(), c != EOF && c != '\n' && c != ' ') putchar(c);
+			p.dyed = DYE_PID;
+			p.dye = DYE_NONE;
+			goto again;
 		}
 
 		/* integer (constant) */
 		if(c >= '0' && c <= '9') {
-			p.ctx = CTX_INT;
-			p.dye = DYE_INT;
-			continue;
+			printf(DYE_INT "%c", c);
+			while(p.prev = c, c = getchar(), c != EOF && c != '\n' && ((c >= '0' && c <= '9') || c == 'x' || (c >= 'a' && c <= 'f'))) putchar(c);
+			p.dyed = DYE_INT;
+			p.dye = DYE_NONE;
+			goto again;
 		}
 
 		/* enumeration (constant) */
 		if(((c >= 'A' && c <= 'Z') || c == '_') && !((p.prev >= 'a' && p.prev <= 'z') || (p.prev >= '0' && p.prev <= '9'))) {
-			p.ctx = CTX_ENUM;
-			p.dye = DYE_ENUM;
-			continue;
+			printf(DYE_ENUM "%c", c);
+			while(p.prev = c, c = getchar(), c != EOF && c != '\n' && ((c >= 'A' && c <= 'Z') || c == '_')) putchar(c);
+			p.dyed = DYE_ENUM;
+			p.dye = DYE_NONE;
+			goto again;
 		}
 
 		/* comments */
 		if(c == '/') {
 			c = getchar();
 			if(c == '*') {
-				p.dye = DYE_CMNT;
-				p.ctx = CTX_CMNT;
-				ungetc(c, stdin);
-				c = '/';
-				continue;
+				printf(DYE_CMNT "/*");
+				while(p.prev = c, c = getchar(), c != EOF && (c != '/' || p.prev != '*')) putchar(c);
+				p.prev = putchar(c);
+				c = getchar();
+				p.dyed = DYE_CMNT;
+				p.dye = DYE_NONE;
+				goto again;
 			} else {
 				ungetc(c, stdin);
 				c = '/';
@@ -138,17 +118,28 @@ noctx:
 
 		/* mark */
 		if(c == '(' || c == ')' || c == '[' || c == ']' || c == '{' || c == '}') {
-			p.ctx = CTX_MARK;
-			p.dye = DYE_MARK;
-			continue;
+			printf(DYE_MARK "%c", c);
+			p.prev = c;
+			c = getchar();
+			p.dyed = DYE_MARK;
+			p.dye = DYE_NONE;
+			goto again;
 		}
 
 		/* string (literal) */
 		if(c == '"') {
-			p.ctx = CTX_STR;
-			p.had |= HAD_ESC;
-			p.dye = DYE_STR;
-			continue;
+			printf(DYE_STR "\"");
+			for(
+					bool had_esc = 1;
+					c = getchar(), c != EOF && c != '\n' && (c != '"' || had_esc);
+					putchar(c)) {
+				had_esc = c == '\\' ? putchar(c), putchar(c = getchar()), 1: 0;
+			}
+			p.prev = putchar(c);
+			c = getchar();
+			p.dyed = DYE_STR;
+			p.dye = DYE_NONE;
+			goto again;
 		}
 	}
 
