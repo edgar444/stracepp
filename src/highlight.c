2425ad7a169f3b4db027ef94cceee819559cccfd
Edgars Dragon <edgar444@users.noreply.github.com>
2020-08-01 15:07:21 +0300

    src: experiment with splitting the ctx entry from exit

--- src/highlight.c
+++ src/highlight.c
@@ -10,6 +10,7 @@
 #include <stdio.h>    /* getchar fputs */
 #include <stdlib.h>   /* EXIT_SUCCESS */
 #include <stdbool.h>  /* bool */
+#include <assert.h>   /* assert */
 
 #define DYE_NONE  "\033[m"          /* clear           */
 #define DYE_PID   "\033[38;5;110m"  /* cyan            */
@@ -52,50 +53,89 @@ int main(void) {
 			continue;
 		}
 
-again:
+		/* exit context */
+
+		if(!p.ctx) goto noctx;
+
+		if(p.ctx & CTX_CMNT && c == '/' && p.prev == '*') {
+			p.had |= HAD_CMNT;
+			continue;
+		}
+		if(p.ctx & CTX_STR) {
+			if(c == '\\') {
+				p.had |= HAD_ESC;
+				putchar(c);
+				putchar(c = getchar());
+				c = getchar();
+				continue;
+			} else if (p.had & HAD_ESC) {
+				p.had &= ~HAD_ESC;
+				continue;
+			}
+		}
+
+		if(p.ctx & CTX_MARK) {
+			p.ctx &= ~CTX_MARK;
+			p.dye = DYE_NONE;
+		} else if(p.ctx & CTX_INT && !((c >= '0' && c <= '9') || c == 'x' || (c >= 'a' && c <= 'f'))) {
+			p.ctx &= ~CTX_INT;
+			p.dye = DYE_NONE;
+		} else if(p.ctx & CTX_ENUM && !((c >= 'A' && c <= 'Z') || c == '_')) {
+			p.ctx &= ~CTX_ENUM;
+			p.dye = DYE_NONE;
+		} else if(p.ctx & CTX_STR && p.prev == '"' && ~p.had & HAD_ESC) {
+			p.ctx &= ~CTX_STR;
+			p.dye = DYE_NONE;
+		} else if(p.ctx & CTX_CMNT && p.had & HAD_CMNT) {
+			p.ctx &= ~CTX_CMNT;
+			p.had &= ~HAD_CMNT;
+			p.dye = DYE_NONE;
+		} else if(p.ctx & CTX_SYS && p.had & HAD_SYS && !((c >= 'a' && c <= 'z') || (c >= '0' && c <= '9') || c == '_')) {
+			p.ctx &= ~CTX_SYS;
+			p.dye = DYE_NONE;
+		} else if(p.ctx & CTX_PID && c == ' ') {
+			p.ctx &= ~CTX_PID;
+			p.dye = DYE_NONE;
+		} else {
+			continue;
+		}
+
+
+		/* enter context */
+noctx:
+		assert(!p.ctx);
+
 		/* syscall */
-		if(~p.had & HAD_SYS && ~p.ctx & CTX_SYS && ((c >= 'a' && c <= 'z') || c == '_') && p.prev == ' ') {
+		if(~p.had & HAD_SYS && ((c >= 'a' && c <= 'z') || c == '_') && p.prev == ' ') {
 			p.ctx |= CTX_SYS;
 			p.had |= HAD_SYS;
 			p.dye = DYE_SYS;
 			continue;
 		} else if(~p.had & HAD_SYS && ((c == p.prev && c == '+') || c == '-')) {
 			p.had |= HAD_SYS;
-		} else if(p.had & HAD_SYS && p.ctx & CTX_SYS && !((c >= 'a' && c <= 'z') || (c >= '0' && c <= '9') || c == '_')) {
-			p.dye = DYE_NONE;
-			p.ctx &= ~CTX_SYS;
-			goto again;
 		}
 
 		/* pid (field 1) */
 		if(~p.had & HAD_PID && (p.had |= HAD_PID)) {
 			if(c >= '0' && c <= '9' && (p.ctx |= CTX_PID))
 				p.dye = DYE_PID;
-		} else if(p.ctx & CTX_PID && c == ' ' && (p.ctx &= ~CTX_PID, 1)) {
-			p.dye = DYE_NONE;
-			goto again;
 		}
 
 		/* integer (constant) */
 		if(!p.ctx && c >= '0' && c <= '9' && (p.ctx |= CTX_INT)) {
 			p.dye = DYE_INT;
 			continue;
-		} else if(p.ctx & CTX_INT && (c < '0' || c > '9') && c != 'x' && (c < 'a' || c > 'f') && (p.ctx &= ~CTX_INT, 1)) {
-			p.dye = DYE_NONE;
-			goto again;
 		}
 
 		/* enumeration (constant) */
-		if(!p.ctx && ((c >= 'A' && c <= 'Z') || c == '_') && !(p.prev >= 'a' && p.prev <= 'z') && !(p.prev >= '0' && p.prev <= '9') && (p.ctx |= CTX_ENUM)) {
+		if(((c >= 'A' && c <= 'Z') || c == '_') && !((p.prev >= 'a' && p.prev <= 'z') || (p.prev >= '0' && p.prev <= '9'))) {
+			p.ctx |= CTX_ENUM;
 			p.dye = DYE_ENUM;
 			continue;
-		} else if(p.ctx & CTX_ENUM && (c < 'A' || c > 'Z') && c != '_' && (p.ctx &= ~CTX_ENUM, 1)) {
-			p.dye = DYE_NONE;
-			goto again;
 		}
 
 		/* comments */
-		if(!p.ctx && c == '/') {
+		if(c == '/') {
 			c = getchar();
 			if(c == '*') {
 				p.dye = DYE_CMNT;
@@ -107,41 +147,21 @@ again:
 				ungetc(c, stdin);
 				c = '/';
 			}
-		} else if(p.ctx & CTX_CMNT && c == '/' && p.prev == '*') {
-			p.had |= HAD_CMNT;
-		} else if(p.ctx & CTX_CMNT && p.had & HAD_CMNT) {
-			p.ctx &= ~CTX_CMNT;
-			p.had &= ~HAD_CMNT;
-			p.dye = DYE_NONE;
-			goto again;
 		}
 
 		/* mark */
-		if(!p.ctx && (c == '(' || c == ')' || c == '[' || c == ']' || c == '{' || c == '}') && (p.ctx |= CTX_MARK)) {
+		if(c == '(' || c == ')' || c == '[' || c == ']' || c == '{' || c == '}') {
+			p.ctx |= CTX_MARK;
 			p.dye = DYE_MARK;
 			continue;
-		} else if(p.ctx & CTX_MARK && (p.ctx &= ~CTX_MARK, 1)) {
-			p.dye = DYE_NONE;
-			goto again;
 		}
 
 		/* string (literal) */
-		if(!p.ctx && c == '"' && (p.ctx |= CTX_STR)) {
+		if(c == '"') {
+			p.ctx |= CTX_STR;
+			p.had |= HAD_ESC;
 			p.dye = DYE_STR;
-			p.had |= HAD_ESC;
 			continue;
-		} else if(p.ctx & CTX_STR && c == '\\') {
-			p.had |= HAD_ESC;
-			putchar(c);
-			putchar(c = getchar());
-			c = getchar();
-			continue;
-		} else if(p.ctx & CTX_STR && p.had & HAD_ESC) {
-			p.had &= ~HAD_ESC;
-			continue;
-		} else if(p.ctx & CTX_STR && p.prev == '"' && (p.ctx &= ~CTX_STR, 1)) {
-			p.dye = DYE_NONE;
-			goto again;
 		}
 	}
 
