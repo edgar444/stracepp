72130498c8d2042b565828f6b0a2ad44236c4aa9
Edgars Dragon <edgar444@users.noreply.github.com>
2020-08-01 20:53:07 +0300

    test: metafields permutations via getpid pure syscall

---
+++ test/syntax/metafields/03_all.c
@@ -0,0 +1,11 @@
+//bin/true; WFLAGS="-Wall -Wextra -Wpedantic -Wcast-align=strict -Wcast-qual -Wshadow -Wsign-conversion -Wunused-macros -Wwrite-strings"
+//bin/true; o=/tmp/stracepp_test.d/$(md5sum "$0" | cut -f1 -d' ');
+//bin/true; cc=(cc -std=iso9899:2018 -ggdb3 $WFLAGS -o $o "$0")
+//bin/true; mkdir -p ${o%/*} && [ -x $o ] || "${cc[@]}" || exit 1
+//bin/true; env -i "$(which setarch)" -R "$(which strace)" -Ttttfirqo /dev/stdout -e getpid $o
+//bin/true; exit 1
+#include <unistd.h>
+int main(void) {
+	getpid();
+	return 0;
+}
